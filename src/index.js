7213c7f1b6290c4d793fa4adaf4a4165f1de8114
hongntbhtech <hongnt.bhsoft@gmail.com>
2024-10-10 08:53:17 +0700

    deploy vercel1

--- src/index.js
+++ src/index.js
@@ -1,16 +1,47 @@
+// const Routes = require('./routes/routers');
+// const express = require('express');
+// const path = require('path');
+// const cors = require('cors');
+
+// const PORT = 4001
+// const corsOptions = {
+//     origin: 'https://final-test-ashy-nu.vercel.app',
+//     optionsSuccessStatus: 200
+// };
+
+// const app = express();
+// app.use(cors());
+// app.use(express.json());
+
+// app.use(express.static(path.join(__dirname, 'src/routes')));
+
+// new Routes(app);
+
+// app.use((req, res) => {
+//     res.status(404).send({
+//         status: 404,
+//         message: 'Error, Page was not found.'
+//     });
+// });
+
+
+// app.listen(PORT, () => {
+//     console.log(`Connecting Server http://localhost:${PORT}`);
+// });
+
+
 const Routes = require('./routes/routers');
 const express = require('express');
 const path = require('path');
 const cors = require('cors');
 
-const PORT = 4001
 const corsOptions = {
     origin: 'https://final-test-ashy-nu.vercel.app',
     optionsSuccessStatus: 200
 };
 
 const app = express();
-app.use(cors());
+app.use(cors(corsOptions)); // Sử dụng corsOptions
 app.use(express.json());
 
 app.use(express.static(path.join(__dirname, 'src/routes')));
@@ -24,7 +55,5 @@ app.use((req, res) => {
     });
 });
 
-
-app.listen(PORT, () => {
-    console.log(`Connecting Server http://localhost:${PORT}`);
-});
+// Xuất ứng dụng
+module.exports = app;
