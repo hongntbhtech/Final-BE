de678434e0c2a3f0a259f141f79c9b68ede06b2e
hongntbhtech <hongnt.bhsoft@gmail.com>
2024-10-10 09:41:13 +0700

    deplou vercel3

--- server.js
+++
@@ -1,6 +0,0 @@
-const app = require('./app'); // Thay thế './app' bằng đường dẫn tới tệp của bạn
-const PORT = process.env.PORT || 4001; // Lấy cổng từ biến môi trường hoặc mặc định là 4001
-
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-});
--- src/index.js
+++ src/index.js
@@ -1,47 +1,16 @@
-// const Routes = require('./routes/routers');
-// const express = require('express');
-// const path = require('path');
-// const cors = require('cors');
-
-// const PORT = 4001
-// const corsOptions = {
-//     origin: 'https://final-test-ashy-nu.vercel.app',
-//     optionsSuccessStatus: 200
-// };
-
-// const app = express();
-// app.use(cors());
-// app.use(express.json());
-
-// app.use(express.static(path.join(__dirname, 'src/routes')));
-
-// new Routes(app);
-
-// app.use((req, res) => {
-//     res.status(404).send({
-//         status: 404,
-//         message: 'Error, Page was not found.'
-//     });
-// });
-
-
-// app.listen(PORT, () => {
-//     console.log(`Connecting Server http://localhost:${PORT}`);
-// });
-
-
 const Routes = require('./routes/routers');
 const express = require('express');
 const path = require('path');
 const cors = require('cors');
 
+const PORT = 4001
 const corsOptions = {
     origin: 'https://final-test-ashy-nu.vercel.app',
     optionsSuccessStatus: 200
 };
 
 const app = express();
-app.use(cors(corsOptions)); // Sử dụng corsOptions
+app.use(cors());
 app.use(express.json());
 
 app.use(express.static(path.join(__dirname, 'src/routes')));
@@ -55,5 +24,7 @@ app.use((req, res) => {
     });
 });
 
-// Xuất ứng dụng
-module.exports = app;
+
+app.listen(PORT, () => {
+    console.log(`Connecting Server http://localhost:${PORT}`);
+});
